3a0be596903f15af533ad88906aecab8e2226aa7
Ahmed <ahmed.marzook@cgi.com>
2018-09-27 11:18:16 +0000

    Completed paired Fizzbuzz Exercise with @ben

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,13 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed a number divisible by 3 && 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "number" when passed a number not divisible by 3 || 5' do
+    expect(fizzbuzz(46)).to eq 46
+  end
 end
